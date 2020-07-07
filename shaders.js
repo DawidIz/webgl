23b04f90c8883be1cd0719d45245605838191b8e
DawidIz <41062760+DawidIz@users.noreply.github.com>
2020-07-07 19:26:52 +0200

    Add files via upload

--- shaders.js
+++ shaders.js
@@ -1,17 +1,26 @@
-const vertex = `
-attribute vec4 aVertexPosition;
+// const vertexShaderSource = `
+// attribute vec4 aVertexPosition;
 
-uniform mat4 uModelViewMatrix;
-uniform mat4 uProjectionMatrix;
+// uniform mat4 uModelViewMatrix;
+// uniform mat4 uProjectionMatrix;
+
+// void main() {
+//   gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+// }
+// `
+
+const vertexShaderSource = `
+attribute vec4 a_position;
 
 void main() {
-  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  gl_Position = a_position;
 }
 `
 
-const fragment = `
+const fragmentShaderSource = `
+//precision mediump float;
 void main() {
-  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  gl_FragColor = vec4(1.0, 0.2, 1.0, 1.0);
 }
 `
-export { vertex, fragment }
+export { vertexShaderSource, fragmentShaderSource }
